f0273437d345b53c417d52ffc546f547f1f3c5fb
Ian Denhardt <ian@zenhack.net>
2021-01-11 04:44:26 -0500

    foreign import c_open{,at} minor cleanups.

--- src/Unix/C.hs
+++ src/Unix/C.hs
@@ -1,8 +1,11 @@
 module Unix.C
     ( module X
+    , CStr(..)
     , c_fdatasync
     , c_fsync
     , c_ftruncate
+    , c_open
+    , c_openat
     , c_pread
     , c_pwrite
     , c_read
@@ -15,10 +18,14 @@ import Foreign.C.Types    as X
 import Foreign.Ptr        as X
 import System.Posix.Types as X
 
-foreign import ccall "fsync" c_fsync :: Fd -> IO Int
-foreign import ccall "fdatasync" c_fdatasync :: Fd -> IO Int
+newtype CStr = CStr (Ptr CChar)
+
+foreign import ccall "fdatasync" c_fdatasync :: Fd -> IO CInt
+foreign import ccall "fsync" c_fsync :: Fd -> IO CInt
 foreign import ccall "ftruncate" c_ftruncate :: Fd -> COff -> IO Int
-foreign import ccall "read"  c_read  :: Fd -> Ptr Word8 -> CSize -> IO CSsize
-foreign import ccall "write" c_write :: Fd -> Ptr Word8 -> CSize -> IO CSsize
+foreign import ccall "openat" c_openat :: Fd -> CStr -> CInt -> CMode -> IO Int
+foreign import ccall "open" c_open :: CStr -> CInt -> CMode -> IO Int
 foreign import ccall "pread"  c_pread  :: Fd -> Ptr Word8 -> CSize -> COff -> IO CSsize
 foreign import ccall "pwrite" c_pwrite :: Fd -> Ptr Word8 -> CSize -> COff -> IO CSsize
+foreign import ccall "read"  c_read  :: Fd -> Ptr Word8 -> CSize -> IO CSsize
+foreign import ccall "write" c_write :: Fd -> Ptr Word8 -> CSize -> IO CSsize
